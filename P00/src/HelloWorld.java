de96c0e88ff5c66b0398ca70aa683274ec77021d
roychiam <roychiam@192.168.18.23>
2022-04-25 22:27:03 +0800

    first push

---
+++ P00/src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public String announce() {
+		return "My First Java Program";
+	}
+}
---
+++ P00/test/HelloWorldTest.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		HelloWorld h = new HelloWorld();
+		String str1 = "My First Java Program";
+		String str2 = h.announce();
+		assertEquals(str1, str2);
+	}
+
+}
